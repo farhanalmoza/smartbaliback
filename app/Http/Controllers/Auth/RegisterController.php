fd726001cbf4dda5aad2d4e06a72825ecf3cae54
Farhan <oyisamfarhan@gmail.com>
2021-07-13 19:24:41 +0700

    register without verify email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Models\Profile;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -68,6 +69,15 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+        ]);
+    }
+
+    protected function createProfile(array $data)
+    {
+        $user = User::where('email', $data['email'])->first();
+        return Profile::create([
+            'name' => $data['name'],
+            'email' => $data['email']
         ]);
     }
 }
